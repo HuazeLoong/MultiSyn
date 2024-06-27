365c375201ef3175f483542fc945cf4dd16f2e11
DraNaleLoong <50662082+Drachze@users.noreply.github.com>
2024-06-27 11:03:45 +0800

    Update const.py

--- const.py
+++ const.py
@@ -8,6 +8,7 @@ RESULT_DIR = os.path.join(DATA_DIR,'result')
 RESULTS_DIR = os.path.join(RESULT_DIR,'results')
 LOSS_DIR = os.path.join(RESULT_DIR,'loss')
 
+DRUGID_DIR = os.path.join(DATA_DIR, 'drug_id.csv')
 CELL_DIR = os.path.join(DATA_DIR, 'cell_features.csv')
 SMILES_DIR = os.path.join(DATA_DIR, 'smiles.csv')
 
